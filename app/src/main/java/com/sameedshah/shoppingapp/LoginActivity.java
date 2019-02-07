7fe68fcb105025afc1f7753a465bd0efc87d3bfb
Sameed Shah <sameedshah8@gmail.com>
2019-02-07 20:59:18 +0500

    Login & register activity done!

--- app/src/main/java/com/sameedshah/shoppingapp/LoginActivity.java
+++ app/src/main/java/com/sameedshah/shoppingapp/LoginActivity.java
@@ -1,13 +1,33 @@
 package com.sameedshah.shoppingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    TextView regText;
+    EditText edtUsername;
+    EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        regText = findViewById(R.id.txtRegister);
+        regText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,SignUpActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/sameedshah/shoppingapp/SignUpActivity.java
+++ app/src/main/java/com/sameedshah/shoppingapp/SignUpActivity.java
@@ -1,13 +1,38 @@
 package com.sameedshah.shoppingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.hbb20.CountryCodePicker;
+
+import org.w3c.dom.Text;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    CountryCodePicker codePicker;
+    TextView txtLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        txtLogin = findViewById(R.id.txtLogin);
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignUpActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+
+//        picker = findViewById(R.id.ccPicker);
+//        Toast.makeText(this, picker.getDefaultCountryCode().toString(), Toast.LENGTH_SHORT).show();
     }
 }
